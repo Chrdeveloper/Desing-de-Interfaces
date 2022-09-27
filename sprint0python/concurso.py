d8e9de2255b41527fcf59c3341847a2b2d88a245
Christian <cmartinezguzman@fpcoruna.afundacion.org>
2022-09-27 10:23:18 +0200

    Tarea 7 terminada

--- sprint0python/concurso.py
+++ sprint0python/concurso.py
@@ -1,31 +1,52 @@
+import random
 
-print("Cuantos titulos mundiales tiene Ayrton Senna?")
-print("a.- 4")
-print("b.- 7")
-print("c.- 3")
-respuesta = input("Elige opcion (a/b/c)")
-puntuacion = 0 
-if(respuesta == "c"):
-	puntuacion += 10
-else:
-	puntuacion -=5
-print("Donde gano Mechael Schumaquer su ultima carrera?")
-print("a.- Brasil")
-print("b.- China")
-print("c.- Argentina")
-respuesta = input("Elige opcion (a/b/c)")
-if(respuesta == "b"):
-	puntuacion += 10
-else:
-	puntuacion -=5
+def pregunta1():
+	print("Cuantos titulos mundiales tiene Ayrton Senna?")
+	print("a.- 4")
+	print("b.- 7")
+	print("c.- 3")
+	respuesta = input("Elige opcion (a/b/c)")
+	if(respuesta == "c"):
+		puntuacion = 10
+	else:
+		puntuacion = -5
+	return puntuacion
+	
+def pregunta2():
+	print("Donde gano Mechael Schumaquer su ultima carrera?")
+	print("a.- Brasil")
+	print("b.- China")
+	print("c.- Argentina")
+	respuesta = input("Elige opcion (a/b/c)")
+	if(respuesta == "b"):
+		puntuacion = 10
+	else:
+		puntuacion = -5
+	return puntuacion
+
+def pregunta3():
 	print("Cual es el unico piloto en haber ganado un mundial sin hacer poles")
-print("a.- Ayrton Senna")
-print("b.- Niki Lauda")
-print("c.- Juan Pablo Montoya")
-respuesta = input("Elige opcion (a/b/c)")
-if(respuesta == "b"):
-	puntuacion += 10
-else:
-	puntuacion -=5
-
-print("Tu puntuacion final es: "+ str(puntuacion))	
+	print("a.- Ayrton Senna")
+	print("b.- Niki Lauda")
+	print("c.- Juan Pablo Montoya")
+	respuesta = input("Elige opcion (a/b/c)")
+	if(respuesta == "b"):
+		puntuacion = 10
+	else:
+		puntuacion = -5
+	return puntuacion
+	
+print("Seleccion aleatoria de preguntas")
+intentos = 0
+resultado = 0
+while(intentos <= 1):
+	n = random.randint(1,3)
+	if(n == 1):
+		resultado += pregunta1()
+	if(n == 2):
+		resultado += pregunta2()
+	if(n == 3):
+		resultado += pregunta3()
+	intentos += 1
+	
+print("Resultado es...."+str(resultado))
