d3c7192c5c9a86af2c9a2a0f404885e4e48def1b
Christian <cmartinezguzman@fpcoruna.afundacion.org>
2022-10-10 16:39:53 +0200

    Ejercicio 1

---
+++ extraPython1/extraPython1.py
@@ -0,0 +1,10 @@
+import numpy
+
+def max(a, b):
+    if (b > a):
+        return b
+    else:
+        return a
+
+
+
--- sprint1gtk/catalog/detail_window.py
+++ sprint1gtk/catalog/detail_window.py
@@ -5,16 +5,11 @@ from gi.repository import Gtk
 from gi.repository import GdkPixbuf
 
 class SubWindow(Gtk.Window):
-    name = None
-    descripcion = None
-    image = Gtk.Image()
+
 
     def __init__(self, name, image, descripcion):
         super().__init__(title=name)
-        self.name = name
-        self.descripcion = descripcion
-        self.image = image
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        box.pack_start(self.image, True, True, 0)
-        box.pack_start(Gtk.Label(self.descripcion), True, True, 0)
+        box.pack_start(image, True, True, 0)
+        box.pack_start(Gtk.Label(descripcion), True, True, 0)
         self.add(box)
