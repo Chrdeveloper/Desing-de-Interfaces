710d6c3342ea1177564dc723b79f411ea8077928
Christian <cmartinezguzman@fpcoruna.afundacion.org>
2022-10-10 16:40:35 +0200

    Ejercicio 2

--- extraPython1/extraPython1.py
+++ extraPython1/extraPython1.py
@@ -6,5 +6,17 @@ def max(a, b):
     else:
         return a
 
+def max_de_tres(a, b, c):
+    if a > b:
+        if a > c:
+            return a
+        else:
+            return c
+    else:
+        if (b > c):
+            return b
+        else:
+            return c
+
 
 
