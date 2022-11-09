ba4619470c7047dc28e73d9d4af2d57ca638e45c
Christian <cmartinezguzman@fpcoruna.afundacion.org>
2022-11-09 10:38:10 +0100

    Avance en el drawer layout, supuesto error en el XML

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,28 +1,81 @@
 package com.example.mycatalog;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class CatalogActivity extends AppCompatActivity {
-    private Context context = this;
+import com.google.android.material.navigation.NavigationView;
+
+public class CatalogActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    Menu menu;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button navegador = findViewById(R.id.boton2);
-        navegador.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, DetailActivity.class );
-                context.startActivity(intent);
-            }
-        });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView =  findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        menu = navigationView.getMenu();
+        navigationView.getHeaderView(0);
+        navigationView.bringToFront();
+
+        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.open_drawer, R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
 
 
+
+
+        //Button navegador = findViewById(R.id.boton2);
+        //    navegador.setOnClickListener(new View.OnClickListener() {
+        //        @Override
+        //        public void onClick(View view) {
+        //            Intent intent = new Intent(context, DetailActivity.class );
+        //            context.startActivity(intent);
+        //        }
+        //    });
+
+
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.nav_prenda:
+                startActivity(new Intent(CatalogActivity.this, DetailActivity.class));
+                break;
+            case R.id.nav_boton:
+
+        }
+
+
+        return false;
     }
 }
